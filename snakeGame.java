bef09ff530d43f7eb024736a55f2670de2706e78
sakshi yadav <sakshi923456@gmail.com>
2021-03-12 13:52:54 +0530

    snakegame file is modified

--- snakeGame.java
+++ snakeGame.java
@@ -3,3 +3,35 @@
 // crosses it self->game over,
 // Food ->at given interval
 // Classes --> Snake, Cell, Board, Game
+// To represent a cell of display board.
+public class Cell {
+ 
+    private final int row, col;
+    private CellType cellType;
+ 
+    public Cell(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+ 
+    public CellType getCellType()
+    {
+        return cellType;
+    }
+ 
+    public void setCellType(CellType cellType)
+    {
+        this.cellType = cellType;
+    }
+ 
+    public int getRow()
+    {
+        return row;
+    }
+ 
+    public int getCol()
+    {
+        return col;
+    }
+}
